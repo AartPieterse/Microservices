70da9a8757a9c808f217965848558f9a48631fe1
Niels Kloos <nielskloos2001@gmail.com>
2023-07-01 21:22:48 +0200

    test-program documentation geschreven

--- apps/test-program/src/commands/create-potentialTest.command.handler.ts
+++ apps/test-program/src/commands/create-potentialTest.command.handler.ts
@@ -2,30 +2,38 @@ import { CommandHandler, EventPublisher, ICommandHandler } from "@nestjs/cqrs";
 import { CreatePotentialTestCommand } from "./create-potentialTest.command";
 import { PotentialTestRegisteredEvent } from "../events/potentialTest-registered.event";
 import { TestProgramRepository } from "../test-program.repository";
+
+/**
+ * Command handler class responsible for handling the CreatePotentialTestCommand.
+ */
 @CommandHandler(CreatePotentialTestCommand)
 export class CreatePotentialTestCommandHandler implements ICommandHandler<CreatePotentialTestCommand> {
-
     constructor(
-        
-        private readonly publisher: EventPublisher, 
+        /**
+         * The event publisher used to publish events.
+         */
+        private readonly publisher: EventPublisher,
+        /**
+         * The repository for managing test programs.
+         */
         private testProgramRepository: TestProgramRepository
-        ){}
+    ) {}
 
+    /**
+     * Executes the command handler logic.
+     * @param command The CreatePotentialTestCommand instance.
+     * @returns A promise that resolves to the result of the command execution.
+     */
     async execute(command: CreatePotentialTestCommand): Promise<any> {
-        console.log("Triggered Command Handler")
-
+        console.log("Triggered Command Handler");
 
         const { createPotentialTestDto } = command;
 
-        // Saving test into database
+        // Saving the test into the database
         const potentialTest = await this.testProgramRepository.create(createPotentialTestDto);
         const test = new PotentialTestRegisteredEvent(potentialTest);
 
-       
         const event = this.publisher.mergeObjectContext(test);
         event.commit();
     }
-    
 }
-
-
--- apps/test-program/src/commands/create-potentialTest.command.ts
+++ apps/test-program/src/commands/create-potentialTest.command.ts
@@ -1,8 +1,13 @@
 import { ICommand } from "@nestjs/cqrs";
 import { createPotentialTestDto } from "../dto/create-potentialTest.dto";
 
-export class CreatePotentialTestCommand implements ICommand{
-    constructor( public readonly createPotentialTestDto: createPotentialTestDto,
-    ) {}
-  }
-  
+/**
+ * Command class for creating a potential test.
+ */
+export class CreatePotentialTestCommand implements ICommand {
+    /**
+     * Creates an instance of CreatePotentialTestCommand.
+     * @param createPotentialTestDto The DTO object containing the data for creating a potential test.
+     */
+    constructor(public readonly createPotentialTestDto: createPotentialTestDto) {}
+}
